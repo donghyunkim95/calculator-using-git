faa4f5074091904fd19a13245580a59ebfa0cd82
donghyunkim <ltll664@gmail.com>
2023-10-25 13:10:48 +0900

    feature/modulo branch first commit - modulo() 작성

--- src/Main.java
+++ src/Main.java
@@ -71,5 +71,6 @@ public class Main {
     private static void divide(int a, int b) {
     }
     private static void modulo(int a, int b) {
+        System.out.println(a + "%" + b + " = " + (a % b));
     }
 }
